3f596eee29fb191b98e1054f87ed097345afae4c
shahvir-w <shahvir05@icloud.com>
2025-02-06 03:54:40 -0500

    completed basic ui for FlightDetails

--- frontend/src/components/FlightDetails.tsx
+++ frontend/src/components/FlightDetails.tsx
@@ -3,6 +3,7 @@ import { FaSearch } from "react-icons/fa";
 import { GiCommercialAirplane } from "react-icons/gi";
 import { GoDotFill } from "react-icons/go";
 import { Label } from 'semantic-ui-react'
+import { FaPlane, FaTachometerAlt, FaCloud, FaClock, FaRoute, FaMapMarkerAlt } from "react-icons/fa";
 
 
 const FlightDetails = () => {
@@ -24,6 +25,8 @@ const FlightDetails = () => {
         <span className={styles.airlineName}>Air Canada</span>
         <span className={styles.separator}> / </span>
         <span className={styles.flightNumber}>AC1976</span>
+        <span className={styles.separator}> / </span>
+        <span className={styles.status}>On Time</span>
       </div>
 
       {/* Flight details */}
@@ -59,40 +62,116 @@ const FlightDetails = () => {
 
       <div className={styles.timingData}>
         {/* Departure Section */}
-        <div className={styles.departureContainer}>
+        <div className={styles.departure_arrivalContainer}>
+          
           <div className={styles.date}>
-           <h3 className={styles.sectionTitle}>Departure</h3>
-           <span className={styles.data}>Feb 10, 2025</span>
+           <h3 className={styles.sectionTitle}>DEPERTURE</h3>
+           <span className={styles.dateText}>05-Feb-2025</span>
           </div>
          
           
-          <div className={styles.flightInfo}>
+          <div className={styles.row}>
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Scheduled</span>
+              <span className={styles.sectionText}>08:00</span>
+            </div>
             
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Actual</span>
+              <span className={styles.sectionText}>08:15</span>
+            </div>
           </div>
           
-          
-          <div className={styles.flightInfo}>
-            <span className={styles.label1}>Scheduled</span>
-            <span className={styles.data}>08:00</span>
-          </div>
-          
-          <div className={styles.flightInfo}>
-            <span className={styles.label1}>Actual</span>
-            <span className={styles.data}>08:15</span>
-          </div>
-          
-          <div className={styles.flightInfo}>
-            <span className={styles.label1}>Terminal</span>
-            <span className={styles.data}>3</span>
-          </div>
-          
-          <div className={styles.flightInfo}>
-            <span className={styles.label1}>Gate</span>
-            <span className={styles.data}>F12</span>
+          <div className={styles.row1}>
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Terminal</span>
+              <span className={styles.sectionText}>1</span>
+            </div>
+            
+            <div className={styles.pair1}>
+              <span className={styles.sectionTitle}>Gate</span>
+              <span className={styles.sectionText}>N/A</span>
+            </div>
           </div>
         </div>
 
-        
+
+        {/* Arrival Section */}
+        <div className={styles.departure_arrivalContainer}>
+          
+          <div className={styles.date}>
+           <h3 className={styles.sectionTitle}>ARRIVAL</h3>
+           <span className={styles.dateText}>05-Feb-2025</span>
+          </div>
+         
+          
+          <div className={styles.row}>
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Scheduled</span>
+              <span className={styles.sectionText}>08:00</span>
+            </div>
+            
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Estimated</span>
+              <span className={styles.sectionText}>08:15</span>
+            </div>
+          </div>
+          
+          <div className={styles.row1}>
+            <div className={styles.pair}>
+              <span className={styles.sectionTitle}>Terminal</span>
+              <span className={styles.sectionText}>1</span>
+            </div>
+            
+            <div className={styles.pair1}>
+              <span className={styles.sectionTitle}>Gate</span>
+              <span className={styles.sectionText}>N/A</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className={styles.flightStats}>
+        <div className={styles.col}>
+          <div className={styles.statItem}>
+            <FaPlane className={styles.icon} />
+            <span className={styles.label2}>Aircraft:</span>
+            <span className={styles.value}>Boeing 787</span>
+          </div>
+
+          <div className={styles.statItem}>
+            <FaTachometerAlt className={styles.icon} />
+            <span className={styles.label2}>Speed:</span>
+            <span className={styles.value}>870 km/h</span>
+          </div>
+
+          <div className={styles.statItem}>
+            <FaCloud className={styles.icon} />
+            <span className={styles.label2}>Altitude:</span>
+            <span className={styles.value}>35,000 ft</span>
+         </div>
+        </div>
+
+        <div className={styles.col}>
+          <div className={styles.statItem}>
+            <FaClock className={styles.icon} />
+            <span className={styles.label2}>Total Time:</span>
+            <span className={styles.value}>8h 15m</span>
+          </div>
+
+          <div className={styles.statItem}>
+            <FaRoute className={styles.icon} />
+            <span className={styles.label2}>Dist. Total:</span>
+            <span className={styles.value}>6,500 km</span>
+          </div>
+
+          <div className={styles.statItem}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <span className={styles.label2}>Dist. Left:</span>
+            <span className={styles.value}>1,200 km</span>
+          </div>
+        </div>
       </div>
 
     </div>
